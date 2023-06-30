a5a01e9ea49987452ddb580efb38d37a2317083a
Godwin Pinto <godwin.pinto86@gmail.com>
2023-06-30 16:56:19 +0530

    changes client pam config

--- client/passage-connect.c
+++ client/passage-connect.c
@@ -10,6 +10,40 @@
 
 GoSlice argcvToSlice(int, const char**);
 
+static int my_conv(int num_msg, const struct pam_message **msg, struct pam_response **resp, void *appdata_ptr) {
+    if (num_msg <= 0 || msg == NULL || resp == NULL) {
+        return PAM_CONV_ERR;
+    }
+
+    // Allocate memory for the response array
+    struct pam_response *response = (struct pam_response *)malloc(sizeof(struct pam_response) * num_msg);
+    if (response == NULL) {
+        return PAM_CONV_ERR;
+    }
+
+    // Process each message
+    for (int i = 0; i < num_msg; i++) {
+        const struct pam_message *message = msg[i];
+        struct pam_response *r = &response[i];
+
+        if (message->msg_style == PAM_PROMPT_ECHO_OFF) {
+            // Prompt for password without echoing
+            r->resp = strdup("mypassword");
+        } else if (message->msg_style == PAM_TEXT_INFO) {
+            // Display informational message
+            printf("%s\n", message->msg);
+            r->resp = NULL;
+        } else {
+            // Unsupported message style
+            free(response);
+            return PAM_CONV_ERR;
+        }
+    }
+
+    *resp = response;
+    return PAM_SUCCESS;
+}
+
 PAM_EXTERN int pam_sm_open_session(pam_handle_t *pamh, int flags, int argc, const char **argv) {
     // Perform any necessary operations when opening a session
     printf("Passage Session opened successfully!\n");
@@ -19,6 +53,8 @@ PAM_EXTERN int pam_sm_open_session(pam_handle_t *pamh, int flags, int argc, cons
 
 PAM_EXTERN int pam_sm_close_session(pam_handle_t *pamh, int flags, int argc, const char **argv) {
     // Perform any necessary operations when closing a session
+    pam_set_item(pamh, PAM_CONV, (const void *)&my_conv);
+
     printf("Passage Session closed successfully!\n");
     return PAM_SUCCESS;
 }
